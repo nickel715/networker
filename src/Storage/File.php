66b031f48a4586048fecfa5792c0bd4cd79bc526
Nicolas Hohm <nickel7152@gmail.com>
2015-02-15 02:12:08 +0100

    add file storage

--- src/Storage/File.php
+++ src/Storage/File.php
@@ -2,6 +2,29 @@
 
 namespace Networker\Storage;
 
+use Networker\StorageInterface;
+
 class File implements StorageInterface
 {
+    protected $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function findAll()
+    {
+        return array_filter(explode(PHP_EOL, file_get_contents($this->path)));
+    }
+
+    public function add($user)
+    {
+        file_put_contents($this->path, $user . PHP_EOL, FILE_APPEND);
+    }
+
+    public function addAll(array $users)
+    {
+        file_put_contents($this->path, implode(PHP_EOL, $users) . PHP_EOL, FILE_APPEND);
+    }
 }
---
+++ test/Networker/Storage/FileTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Networker\Test\Storage;
+
+use Networker\Storage\File;
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+    private $userList;
+    private $userListRaw;
+    private $file = 'users.txt';
+    public function setUp()
+    {
+        $this->cleanupPersistance();
+        $this->sut = new File($this->file);
+        $this->userList = ['octocat', 'tekkub', 'mdo'];
+        $this->userListRaw = 'octocat' . PHP_EOL . 'tekkub' . PHP_EOL . 'mdo' . PHP_EOL;
+    }
+
+    public function tearDown()
+    {
+        $this->cleanupPersistance();
+    }
+
+    public function cleanupPersistance()
+    {
+        if (file_exists($this->file)) {
+            unlink($this->file);
+        }
+    }
+
+    public function testFindAll()
+    {
+        file_put_contents($this->file, $this->userListRaw);
+        $this->assertEquals($this->userList, $this->sut->findAll());
+    }
+
+    public function testAdd()
+    {
+        file_put_contents($this->file, 'octocat' . PHP_EOL);
+        $this->sut->add('tekkub');
+        $this->sut->add('mdo');
+        $this->assertEquals($this->userListRaw, file_get_contents($this->file));
+    }
+
+    public function testAll()
+    {
+        $this->sut->addAll($this->userList);
+        $this->assertEquals($this->userListRaw, file_get_contents($this->file));
+    }
+
+
+    public function testAllExisting()
+    {
+        file_put_contents($this->file, 'octocat' . PHP_EOL);
+        array_shift($this->userList);
+        $this->sut->addAll($this->userList);
+        $this->assertEquals($this->userListRaw, file_get_contents($this->file));
+    }
+}
